f5e0c3b077e6ebd662bc1b9827294fb17e134105
LT <LT@qq.com>
2020-08-10 15:26:36 +0800

    在家完成剩余的签到功能

---
+++ SignIn.java
@@ -0,0 +1,11 @@
+/*
+* @Author: Marte
+* @Date:   2020-08-10 14:59:00
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-08-10 15:24:39
+*/
+
+public class SignIn {
+   private String name;
+   private String sign_time;
+}
